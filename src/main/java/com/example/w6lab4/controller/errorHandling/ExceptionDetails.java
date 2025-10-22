1c217e739bd266fa3d6280aac6d54f97be60f781
SUBHANIQBAL-STUDENT <G00433839@atu.ie>
2025-10-22 15:47:57 +0100

    set all the feild and pramaeters

--- src/main/java/com/example/w6lab4/controller/errorHandling/ExceptionDetails.java
+++ src/main/java/com/example/w6lab4/controller/errorHandling/ExceptionDetails.java
@@ -4,10 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data @NoArgsConstructor @AllArgsConstructor
 public class ExceptionDetails {
     private String feildName;
     private  String feildValue;
+
+    public ExceptionDetails(String feildName, String feildValue) {
+        this.feildName = feildName;
+        this.feildValue = feildValue;
+    }
+
+    public ExceptionDetails() {
+
+    }
+
+    public String getFeildName() {
+        return feildName;
+    }
+
+    public void setFeildName(String feildName) {
+        this.feildName = feildName;
+    }
+
+    public String getFeildValue() {
+        return feildValue;
+    }
+
+    public void setFeildValue(String feildValue) {
+        this.feildValue = feildValue;
+    }
 }
--- src/main/java/com/example/w6lab4/controller/errorHandling/GlobalExceptionHandling.java
+++ src/main/java/com/example/w6lab4/controller/errorHandling/GlobalExceptionHandling.java
@@ -1,21 +1,28 @@
 package com.example.w6lab4.controller.errorHandling;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandling {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ExceptionDetails showErrorDetail(MethodArgumentNotValidException mae)
+    public ResponseEntity<List<ExceptionDetails>> showErrorDetail(MethodArgumentNotValidException mae)
     {
+        List<ExceptionDetails> errorList = new ArrayList<>();
         for(FieldError fieldError : mae.getBindingResult().getFieldErrors()) {
             ExceptionDetails exceptionDetails = new ExceptionDetails();
-            exceptionDetails.setFeildName(mae.getBindingResult().getFieldError().getField());
-            exceptionDetails.setFeildValue(mae.getBindingResult().getFieldError().getDefaultMessage());
+            exceptionDetails.setFeildName(fieldError.getField());
+            exceptionDetails.setFeildValue(fieldError.getDefaultMessage());
+            errorList.add(exceptionDetails);
         }
-        return exceptionDetails;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
     }
 }
